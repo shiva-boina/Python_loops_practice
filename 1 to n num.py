edad944cbc8e54caf22ee7cd48b633626f7b206e
shiva-boina <shivaboina1414@gmail.com>
2025-11-20 10:53:13 +0530

    Add files via upload

---
+++ 1 to n num.py
@@ -0,0 +1,9 @@
+
+#Write a program to print numbers from 1 to n. 
+#Use a loop starting from 1 to n and print each number.
+# Input: n = 5 
+n=5
+for a in range(1,n+1):
+    print(a,end=" ")
+
+# Output: 1 2 3 4 5
