0316a5b4d671fdcd716342039106092038333048
shiva-boina <shivaboina1414@gmail.com>
2025-11-22 19:47:28 +0530

    2m to n.py

---
+++ 2m to n.py
@@ -0,0 +1,11 @@
+
+#Write a program to print numbers from m to n.
+# Loop from m to n and print values.
+# Input: m = 3, n = 7 
+
+m=3
+n=7
+for m in range(m,n+1):
+    print(m,end=" ")
+
+#  Output: 3 4 5 6 7
