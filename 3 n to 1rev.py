ede7f64a6c60e9d05b32e86edf9c659f7c2b3220
shiva-boina <shivaboina1414@gmail.com>
2025-11-23 11:42:19 +0530

    3 n to 1rev.py

---
+++ 3 n to 1rev.py
@@ -0,0 +1,9 @@
+
+#Write a program to print numbers in reverse from n to 1. 
+#Use a loop starting from n and decrement to 1. 
+#Input: n = 5 
+n=5
+while n>=1:
+    print(n,end=" ")
+    n-=1
+#Output: 5 4 3 2 1
