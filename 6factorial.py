2f3a674900c0fa50a7200fb2cf0e90ce32cb8995
shiva-boina <shivaboina1414@gmail.com>
2025-11-26 11:24:24 +0530

    factorial

---
+++ 6factorial.py
@@ -0,0 +1,13 @@
+
+#Write a program to find the factorial of a number.
+#Multiply all numbers from 1 to n. 
+#Input: n = 5 
+
+n=5
+fact=1
+while n>=1:
+    fact*=n
+    n-=1
+print(fact)
+
+#Output: 120
