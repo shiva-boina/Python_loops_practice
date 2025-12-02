ffaac1d81450c335c2adfa8f01cbdba78946ee05
shiva-boina <shivaboina1414@gmail.com>
2025-12-02 10:47:34 +0530

    even num m to n

---
+++ 12evennum m to n.py
@@ -0,0 +1,12 @@
+
+#Print all even numbers between m and n. 
+# Use loop and check if divisible by 2.
+# Input: m = 3, n = 10 
+m=3
+n=10
+for m in range(m,n+1):
+    if m%2==0:
+        print(m,end=" ")
+
+# Output: 4 6 8 10
+
