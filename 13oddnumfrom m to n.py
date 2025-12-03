661c9d2d268af9024bf46d0c27a8b5f007ea3d49
shiva-boina <shivaboina1414@gmail.com>
2025-12-03 13:18:25 +0530

    odd num , to n

---
+++ 13oddnumfrom m to n.py
@@ -0,0 +1,14 @@
+
+#Print all odd numbers between m and n.
+# Check if number % 2 != 0. 
+# Input: m = 3, n = 10 
+
+m=3
+n=10
+for m in range(m,n+1):
+    if m%2!=0:
+        print(m,end=" ")
+
+# Output: 3 5 7 9
+
+
